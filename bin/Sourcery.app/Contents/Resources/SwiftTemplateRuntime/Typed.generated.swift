e8dfe667175a860652860fc67c17a26415e8072f
ainame <s.namai.09@gmail.com>
2017-03-02 01:06:05 +0900

    Add Sourcery binary v0.5.8

---
+++ bin/Sourcery.app/Contents/Resources/SwiftTemplateRuntime/Typed.generated.swift
@@ -0,0 +1,48 @@
+// Generated using Sourcery 0.5.7 — https://github.com/krzysztofzablocki/Sourcery
+// DO NOT EDIT
+
+extension AssociatedValue {
+    var isOptional: Bool { return typeName.isOptional }
+    var isImplicitlyUnwrappedOptional: Bool { return typeName.isImplicitlyUnwrappedOptional }
+    var unwrappedTypeName: String { return typeName.unwrappedTypeName }
+    var actualTypeName: TypeName? { return typeName.actualTypeName ?? typeName }
+    var isTuple: Bool { return typeName.isTuple }
+    var isClosure: Bool { return typeName.isClosure }
+    var isArray: Bool { return typeName.isArray }
+}
+extension MethodParameter {
+    var isOptional: Bool { return typeName.isOptional }
+    var isImplicitlyUnwrappedOptional: Bool { return typeName.isImplicitlyUnwrappedOptional }
+    var unwrappedTypeName: String { return typeName.unwrappedTypeName }
+    var actualTypeName: TypeName? { return typeName.actualTypeName ?? typeName }
+    var isTuple: Bool { return typeName.isTuple }
+    var isClosure: Bool { return typeName.isClosure }
+    var isArray: Bool { return typeName.isArray }
+}
+extension TupleElement {
+    var isOptional: Bool { return typeName.isOptional }
+    var isImplicitlyUnwrappedOptional: Bool { return typeName.isImplicitlyUnwrappedOptional }
+    var unwrappedTypeName: String { return typeName.unwrappedTypeName }
+    var actualTypeName: TypeName? { return typeName.actualTypeName ?? typeName }
+    var isTuple: Bool { return typeName.isTuple }
+    var isClosure: Bool { return typeName.isClosure }
+    var isArray: Bool { return typeName.isArray }
+}
+extension Typealias {
+    var isOptional: Bool { return typeName.isOptional }
+    var isImplicitlyUnwrappedOptional: Bool { return typeName.isImplicitlyUnwrappedOptional }
+    var unwrappedTypeName: String { return typeName.unwrappedTypeName }
+    var actualTypeName: TypeName? { return typeName.actualTypeName ?? typeName }
+    var isTuple: Bool { return typeName.isTuple }
+    var isClosure: Bool { return typeName.isClosure }
+    var isArray: Bool { return typeName.isArray }
+}
+extension Variable {
+    var isOptional: Bool { return typeName.isOptional }
+    var isImplicitlyUnwrappedOptional: Bool { return typeName.isImplicitlyUnwrappedOptional }
+    var unwrappedTypeName: String { return typeName.unwrappedTypeName }
+    var actualTypeName: TypeName? { return typeName.actualTypeName ?? typeName }
+    var isTuple: Bool { return typeName.isTuple }
+    var isClosure: Bool { return typeName.isClosure }
+    var isArray: Bool { return typeName.isArray }
+}
