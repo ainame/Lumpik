147503b3b4ca50bda271ebc06e1169f5142a9a3b
ainame <s.namai.09@gmail.com>
2017-03-02 16:09:16 +0900

    Delete useless optional

--- Sources/Swiftkiq.swift
+++ Sources/Swiftkiq.swift
@@ -1,8 +1,8 @@
 final class Swiftkiq {
-    static var host: String? = "127.0.0.1"
-    static var port: UInt16? = 6379
+    static var host: String = "127.0.0.1"
+    static var port: UInt16 = 6379
     
-    static let store: ListStorable = try! RedisStore(host: host!, port: port!)
+    static let store: ListStorable = try! RedisStore(host: host, port: port)
     
     enum Control: Error {
         case shutdown
