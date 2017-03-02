d1c738f69437e56f898628188b7e4969259d8931
ainame <s.namai.09@gmail.com>
2017-03-02 16:09:03 +0900

    Add manager callback

--- Sources/Manager.swift
+++ Sources/Manager.swift
@@ -7,3 +7,46 @@
 //
 
 import Foundation
+
+protocol ProcessorLifecycleDelegate {
+    func stopped(processor: Processor)
+    func died(processor: Processor, reason: String)
+}
+
+class Manager: ProcessorLifecycleDelegate {
+    let concurrency: Int
+    let processors: [Processor]
+    
+    init(concurrency: Int = 25, queues: [Queue],
+         strategy: Fetcher.Type = BasicFetcher.self, router: Routable) {
+        self.concurrency = concurrency
+        self.processors = (0...concurrency).map { index in
+            DispatchQueue(label: "swiftkiq-queue\(index)")
+        }.map { dispatchQueue in
+            let fetcher = strategy.init(queues: queues)
+            return Processor(fetcher: fetcher, router: router, dispatchQueue: dispatchQueue)
+        }
+    }
+    
+    func start() {
+        processors.forEach { processor in
+            processor.start()
+        }
+    }
+    
+    func stop() {
+        fatalError("not implemented error")
+    }
+    
+    func quit() {
+        fatalError("not implemented error")
+    }
+    
+    func stopped(processor: Processor) {
+        print("stopped: \(processor)")
+    }
+    
+    func died(processor: Processor, reason: String) {
+        print("died: \(processor)")
+    }
+}
