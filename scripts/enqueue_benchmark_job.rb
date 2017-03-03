43492e16b1e0a56dbb442933386f5e738f3425d7
ainame <s.namai.09@gmail.com>
2017-03-03 12:55:42 +0900

    Add benchmark code

---
+++ scripts/enqueue_benchmark_job.rb
@@ -0,0 +1,9 @@
+require 'redis'
+
+redis = Redis.new(host: "127.0.0.1", port: 6379)
+redis.pipelined do |r|
+  100_000.times do |i|
+    value = "{\"jid\": \"aaaa\",\"class\":\"EchoWorker\",\"args\":{\"message\":\"#{i}\"},\"retry\":1,\"queue\":\"default\"}"
+    r.lpush("queue:default", value)
+  end
+end
