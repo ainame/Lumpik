26f4fdc8939da02e63d1cc9d28493d5170a09a8f
ainame <s.namai.09@gmail.com>
2017-03-04 11:24:45 +0900

    Replace Jay to swift-protobuf

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ let package = Package(
     name: "Swiftkiq",
     dependencies: [
         .Package(url: "https://github.com/vapor/redbird.git", majorVersion: 1, minor: 0),
-        .Package(url: "https://github.com/DanToml/Jay.git", majorVersion: 1),
+        .Package(url: "https://github.com/apple/swift-protobuf.git", Version(0, 9, 28)),
         .Package(url: "https://github.com/ainame/Swift-Daemon.git", majorVersion: 0)
     ]
 )
