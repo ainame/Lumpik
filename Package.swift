5685a11138a324b11c286ec28ef8fac70865d270
ainame <s.namai.09@gmail.com>
2017-03-04 13:25:38 +0900

    Delete jay

--- Package.swift
+++ Package.swift
@@ -4,7 +4,6 @@ let package = Package(
     name: "Swiftkiq",
     dependencies: [
         .Package(url: "https://github.com/vapor/redbird.git", majorVersion: 1, minor: 0),
-        .Package(url: "https://github.com/DanToml/Jay.git", majorVersion: 1),
         .Package(url: "https://github.com/ainame/Swift-Daemon.git", majorVersion: 0)
     ]
 )
