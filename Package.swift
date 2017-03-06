5fd77217a6aac37d3b857f4cd125ca7ad548f56b
ainame <s.namai.09@gmail.com>
2017-03-07 01:52:42 +0900

    Add mapper

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@ let package = Package(
     name: "Swiftkiq",
     dependencies: [
         .Package(url: "https://github.com/ainame/Kitura-redis.git", Version(1, 6, 1)),
-        .Package(url: "https://github.com/ainame/Swift-Daemon.git", majorVersion: 0)
+        .Package(url: "https://github.com/ainame/Swift-Daemon.git", majorVersion: 0),
+        .Package(url: "https://github.com/lyft/mapper.git", majorVersion: 6)
     ]
 )
