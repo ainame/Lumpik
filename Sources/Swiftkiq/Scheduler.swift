b16cea221d6ff0417d3b04c0bd848040f6650aa0
ainame <s.namai.09@gmail.com>
2017-03-07 04:01:15 +0900

    WIP Poller

--- Sources/Swiftkiq/Scheduler.swift
+++ Sources/Swiftkiq/Scheduler.swift
@@ -9,17 +9,47 @@
 import Foundation
 
 public class Poller {
-    static let inital_wait = 10
-    
+    static let initalWait: UInt32 = 10
+
+    private let dispatchQueue = DispatchQueue(label: "tokyo.ainame.swiftkiq.poller")
+    private var done: Bool = false
+
     func start() {
+        dispatchQueue.async { [weak self] in
+            self?.run()
+        }
     }
-    
+
     func terminate() {
+        done = true
     }
-    
+
     func enqueue () {
     }
-    
+
+    private func run () {
+        initialWait()
+
+        while !done {
+            enqueue()
+            wait()
+        }
+    }
+
     private func wait() {
+        sleep(randomPollInterval())
+    }
+
+    private func randomPollInterval() -> UInt32 {
+        // poll_interval_average * rand + poll_interval_average.to_f / 2
+        return 1
+    }
+
+    private func initialWait() {
+        // Have all processes sleep between 5-15 seconds. 10 seconds
+        // to give time for the heartbeat to register (if the poll interval is going to be calculated by the number
+        // of workers), and 5 random seconds to ensure they don't all hit Redis at the same time.
+        let total = Poller.initalWait + (5 * Compat.random(1))
+        sleep(total)
     }
 }
