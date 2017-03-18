991b07b129f4bfc402adf9be5731b8a37d69c216
ainame <s.namai.09@gmail.com>
2017-03-19 04:04:12 +0900

    Use uniq jid

--- scripts/enqueue_benchmark_job.rb
+++ scripts/enqueue_benchmark_job.rb
@@ -1,15 +1,16 @@
+require 'securerandom'
 require 'redis'
 
 redis = Redis.new(host: "127.0.0.1", port: 6379)
 redis.pipelined do |r|
   r.del("queue:default")
   50_000.times do |i|
-    value = "{\"jid\": \"aaaa\",\"class\":\"EchoWorker\",\"args\":{\"message\":\"#{i}\"},\"retry\":1,\"queue\":\"default\"}"
+    value = "{\"jid\": \"#{SecureRandom.uuid}\",\"class\":\"EchoWorker\",\"args\":{\"message\":\"#{i}\"},\"retry\":1,\"queue\":\"default\"}"
     r.lpush("queue:default", value)
   end
 
   50_000.times do |i|
-    value = "{\"jid\": \"aaaa\",\"class\":\"EchoWorker\",\"args\":{\"message\":\"#{i}\"},\"retry\":1,\"queue\":\"other\"}"
+    value = "{\"jid\": \"#{SecureRandom.uuid}\",\"class\":\"EchoWorker\",\"args\":{\"message\":\"#{i}\"},\"retry\":1,\"queue\":\"other\"}"
     r.lpush("queue:other", value)
   end
 end
