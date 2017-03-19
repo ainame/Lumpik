f7ffb6c7a13c58dde4686bb586187aea8871cf9a
ainame <s.namai.09@gmail.com>
2017-03-19 12:03:29 +0900

    Pin swift version and package

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "Swiftkiq",
     dependencies: [
-        .Package(url: "https://github.com/vapor/redbird.git", majorVersion: 0),
+        .Package(url: "https://github.com/vapor/redbird.git", Version("2.0.0-alpha.1")!),
         .Package(url: "https://github.com/ainame/Swift-Daemon.git", majorVersion: 0),
         .Package(url: "https://github.com/lyft/mapper.git", majorVersion: 6)
     ]
