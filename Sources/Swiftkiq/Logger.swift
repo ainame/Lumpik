6f340900e7fd907cf73289d3ebce6a86149be71a
ainame <s.namai.09@gmail.com>
2017-05-21 17:21:28 +0800

    Fix default format

--- Sources/Swiftkiq/Logger.swift
+++ Sources/Swiftkiq/Logger.swift
@@ -18,26 +18,32 @@ public struct LoggerInitializer {
         case info = 2
         case warning = 3
         case error = 4
-        
+
         fileprivate var converted: SwiftyBeaver.Level {
             return SwiftyBeaver.Level(rawValue: self.rawValue)!
         }
     }
 
     public static private(set) var isInitialized: Bool = false
-    public static var format = "$DHH:mm:ss.SSS$d $C[$L]$c: $M - $N.$F:$l"
-    
+    public static var format = "[$DYYYY-MM-dd HH:mm:ss.SSS$d]$L: $M"
+
     public static func initialize(loglevel: Loglevel = .debug) {
         guard isInitialized == false else {
             logger.warning("already initialized logger")
             return
         }
-        
+
         let dest = ConsoleDestination()
         dest.minLevel = loglevel.converted
         dest.format = self.format
+        dest.levelString.verbose  = "VERBOSE"
+        dest.levelString.debug    = "  DEBUG"
+        dest.levelString.info     = "   INFO"
+        dest.levelString.warning  = "   WARN"
+        dest.levelString.error    = "  ERROR"
+
         logger.addDestination(dest)
-        
+
         isInitialized = true
     }
 }
