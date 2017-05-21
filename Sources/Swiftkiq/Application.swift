2160b8aa0eb4861b2ef6e3cd9ff53a7696768149
ainame <s.namai.09@gmail.com>
2017-05-21 13:24:45 +0800

    Extract a connection pool from client

--- Sources/Swiftkiq/Application.swift
+++ Sources/Swiftkiq/Application.swift
@@ -7,3 +7,32 @@
 //
 
 import Foundation
+
+struct Application {
+    static var `default`: Application!
+    var connectionPool: ConnectionPool<RedisStore>!
+    
+    static func initialize(launchOptions: LaunchOptions) {
+        guard self.default != nil else {
+            fatalError("don't call Application#initialize twice")
+        }
+        
+        self.default = Application()
+        self.default.connectionPool = ConnectionPool<RedisStore>(maxCapacity: launchOptions.connectionPool)
+    }
+}
+
+// MARK: connection pool
+extension Application {
+    static func connectionPool<T>(handler: (Storable) -> T) throws -> T {
+        return try self.default.connectionPool.with { conn in
+            handler(conn)
+        }
+    }
+    
+    static func connectionPool<T>(handler: (Storable) throws -> T) throws -> T {
+        return try self.default.connectionPool.with { conn in
+            try handler(conn)
+        }
+    }
+}
--- Sources/Swiftkiq/Client.swift
+++ Sources/Swiftkiq/Client.swift
@@ -13,7 +13,7 @@ public struct SwiftkiqClient {
     private static let _connectionPool = ConnectionPool<RedisStore>(maxCapacity: 5)
 
     public static func enqueue<W: Worker, A: Argument>(`class`: W.Type, args: A, retry: Int = W.defaultRetry, to queue: Queue = W.defaultQueue) throws {
-        _ = try SwiftkiqClient.connectionPool { conn in
+        _ = try Application.connectionPool { conn in
             try conn.enqueue(["jid": JobIdentityGenerator.makeIdentity().rawValue,
                               "class": String(describing: `class`),
                               "args": args.toDictionary(),
@@ -25,20 +25,8 @@ public struct SwiftkiqClient {
     public static func enqueue(_ job: [String: Any], to queue: Queue = Queue("default")) throws {
         var newJob = job
         newJob["jid"] = (job["jid"] != nil) ? job["jid"] : JobIdentityGenerator.makeIdentity().rawValue
-        _ = try SwiftkiqClient.connectionPool { conn in
+        _ = try Application.connectionPool { conn in
             try conn.enqueue(newJob, to: queue)
-        }
-    }
-    
-    static func connectionPool<T>(handler: (Storable) -> T) throws -> T {
-        return try _connectionPool.with { conn in
-            handler(conn)
-        }
-    }
-    
-    static func connectionPool<T>(handler: (Storable) throws -> T) throws -> T {
-        return try _connectionPool.with { conn in
-            try handler(conn)
         }
     }
 }
