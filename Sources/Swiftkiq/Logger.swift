29994870225684bee5404d150f75d81a0419967e
ainame <s.namai.09@gmail.com>
2017-05-21 17:25:22 +0800

    Change better format

--- Sources/Swiftkiq/Logger.swift
+++ Sources/Swiftkiq/Logger.swift
@@ -25,6 +25,9 @@ public struct LoggerInitializer {
     }
 
     public static private(set) var isInitialized: Bool = false
+
+    // format sample
+    // [2017-05-21 17:24:20.393]   INFO: jid=eb490cea-bebd-485e-9b31-6c21d3039190 EchoWorker start
     public static var format = "[$DYYYY-MM-dd HH:mm:ss.SSS$d]$L: $M"
 
     public static func initialize(loglevel: Loglevel = .debug) {
