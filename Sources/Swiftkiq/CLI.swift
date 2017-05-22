823ffc3ad6d5ec4a922b945bd68d9ae5bb57be97
ainame <s.namai.09@gmail.com>
2017-05-22 20:00:45 +0800

    Use option parser at start up

--- Sources/Swiftkiq/CLI.swift
+++ Sources/Swiftkiq/CLI.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import Signals
+import Commander
 
 // Signals captured context as a C function
 fileprivate var stopHandler: (()->())? = nil
@@ -15,14 +16,45 @@ fileprivate var quietHandler: (()->())? = nil
 
 public struct CLI {
     private let launcher: Launcher
+    
+    public static func parseOptions(closure: @escaping (LaunchOptions) -> ()) {
+        command(
+            VariadicOption<String>("queue", ["default"], description: "queue name"),
+            Option<Int>("concurrency", 25, description: "the number of threads you want"),
+            Option<String>("pid", "", description: "path of the pid file"),
+            Option<String>("log", "", description: "path of the log file")
+        ) { queues, concurrency, pid, log in
+            var launchOptions = LaunchOptions()
+            launchOptions.queues = queues.map { Queue($0) }
+            launchOptions.concurrency = concurrency
+            
+            if pid != "" {
+                launchOptions.pidfile = URL(fileURLWithPath: pid)
+            }
 
+            if log != "" {
+                launchOptions.logfile = URL(fileURLWithPath: log)
+            }
+
+            closure(launchOptions)
+        }.run()
+    }
+    
+    public static func start(router: Routable) {
+        parseOptions { options in
+            var newOptions = options
+            newOptions.router = router
+            makeCLI(newOptions).start()
+        }
+    }
+    
     public static func makeCLI(_ launchOptions: LaunchOptions) -> CLI {
         Application.initialize(mode: .server, connectionPoolSize: launchOptions.connectionPool)
         LoggerInitializer.initialize(loglevel: launchOptions.loglevel, logfile: launchOptions.logfile)
         let launcher = Launcher.makeLauncher(options: launchOptions)
         return CLI(launcher: launcher)
     }
-
+    
     private init(launcher: Launcher) {
         self.launcher = launcher
     }
