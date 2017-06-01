ed6986cf0e6b3fd22c801bad0e7885317f668401
ainame <s.namai.09@gmail.com>
2017-06-02 00:23:34 +0800

    Add benchmark code

--- Sources/Examples/main.swift
+++ Sources/Examples/main.swift
@@ -3,6 +3,25 @@ import Swiftkiq
 
 var options = LaunchOptions()
 options.connectionPool = 25
-
 let router = Router()
+
+let q = DispatchQueue(label: "benchmark")
+q.async {
+    while true {
+        do {
+            let queue = Queue("default")
+            let count = try queue.count()
+            print("\(queue): \(count)")
+            usleep(50000)
+            if count == 0 {
+                break
+            }
+        } catch {
+            // print(timeout)
+        }
+    }
+    exit(0)
+}
+
+LoggerInitializer.initialize(loglevel: .error, logfile: nil)
 CLI.start(router: router, launchOptions: options)
