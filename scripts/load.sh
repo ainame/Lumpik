1cc003e77c65ca99dd3289abd135e3ca58452b26
ainame <s.namai.09@gmail.com>
2017-06-02 02:13:58 +0800

    Edit Package.swift automaticaly to build Benchmark target

--- scripts/load.sh
+++ scripts/load.sh
@@ -1,4 +1,8 @@
 #!/bin/sh -xe
+sed -e "s/\/\/ Target(name: \"Benchmark\", dependencies: \[\"Swiftkiq\"\]),/Target(name: \"Benchmark\", dependencies: \[\"Swiftkiq\"\]),/" -i "" Package.swift
+sed -e "s/\"Sources\/Benchmark\",/\/\/ \"Sources\/Benchmark\",/" -i "" Package.swift
 swift build -c release
 ruby scripts/enqueue_benchmark_job.rb
 ./.build/release/Benchmark
+sed -e "s/Target(name: \"Benchmark\", dependencies: \[\"Swiftkiq\"\]),/\/\/ Target(name: \"Benchmark\", dependencies: \[\"Swiftkiq\"\]),/" -i "" Package.swift
+sed -e "s/\/\/ \"Sources\/Benchmark\",/\"Sources\/Benchmark\",/" -i "" Package.swift
