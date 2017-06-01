71fdce057d33f0a3a972c26d25c413917a8f2eed
ainame <s.namai.09@gmail.com>
2017-06-02 01:50:45 +0800

    Add metadata output

--- Sources/Benchmark/main.swift
+++ Sources/Benchmark/main.swift
@@ -8,17 +8,22 @@ let router = Router()
 let q = DispatchQueue(label: "benchmark")
 q.async {
     let queue = Queue("default")
+    let start = Date()
     while true {
         do {
             let count = try queue.count()
             print("\(queue): \(count)")
-            usleep(50000)
+            usleep(200000)
             if count == 0 {
                 break
             }
         } catch {
         }
     }
+    
+    let duration = Date().timeIntervalSince(start)
+    let throughput = 100000 / duration
+    print("Done in \(duration): \(throughput) jobs/sec")
     exit(0)
 }
 
