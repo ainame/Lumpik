dad319a402ce915ab54ddc09c2e93f99f42183be
ainame <s.namai.09@gmail.com>
2017-07-27 04:13:14 +0900

    Update packages

--- Package.swift
+++ Package.swift
@@ -1,24 +1,40 @@
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
     name: "Lumpik",
-    targets: [
-    	Target(name: "Lumpik"),
-        // uncomment if you want to build example
-        // Target(name: "Examples", dependencies: ["Lumpik"]),
-        // Target(name: "Benchmark", dependencies: ["Lumpik"]),
+    products: [
+        .library(name: "Lumipik", targets: ["Lumpik"]),
+        .library(name: "LumpikStatic",  type: .static,  targets: ["Lumpik"]),
+        .library(name: "LumpikDynamic", type: .dynamic, targets: ["Lumpik"]),
+        .executable(name: "Examples", targets: ["Examples"]),
+        .executable(name: "Benchmark", targets: ["Benchmark"]),
     ],
     dependencies: [
-        .Package(url: "https://github.com/vapor/redis.git", majorVersion: 2),
-        .Package(url: "https://github.com/ainame/Swift-Daemon.git", majorVersion: 0),
-        .Package(url: "https://github.com/IBM-Swift/BlueSignals.git", majorVersion: 0),
-        .Package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver.git", majorVersion: 1),
-        .Package(url: "https://github.com/kylef/Commander.git", majorVersion: 0),
-        .Package(url: "https://github.com/jpsim/Yams.git", majorVersion: 0)
+        .package(url: "https://github.com/vapor/redis.git", from: "2.0.0"),
+        .package(url: "https://github.com/ainame/Swift-Daemon.git", from: "0.0.1"),
+        .package(url: "https://github.com/IBM-Swift/BlueSignals.git", from: "0.1.0"),
+        .package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver.git", from: "1.0.0"),
+        .package(url: "https://github.com/kylef/Commander.git", from: "0.1.0"),
+        .package(url: "https://github.com/jpsim/Yams.git", from: "0.1.0")
     ],
-    exclude: [
-        // comment out if you want to build example
-        "Sources/Examples",
-        "Sources/Benchmark",
-    ]
+    targets: [
+        .target(name: "Lumpik",
+                dependencies: [
+                    "Redis",
+                    "Daemon",
+                    "Signals",
+                    "SwiftyBeaver",
+                    "Commander",
+                    "Yams"
+                ]),
+        // uncomment if you want to build example
+        .target(name: "Examples", dependencies: ["Lumpik"]),
+        .target(name: "Benchmark", dependencies: ["Lumpik"]),
+    ]// ,
+    // exclude: [
+    //     // comment out if you want to build example
+    //     //     "Sources/Examples",
+    //     //     "Sources/Benchmark",
+    // ]
 )
