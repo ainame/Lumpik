22bf9de1d56bf167e53172e3c75a001f9f8d075c
ainame <s.namai.09@gmail.com>
2017-09-11 02:13:46 +0900

    Expose I/F and use generics

--- Sources/Lumpik/RedisStore.swift
+++ Sources/Lumpik/RedisStore.swift
@@ -36,15 +36,15 @@ public final class RedisStore: Connectable {
     public func releaseConnection() throws {
     }
 
-    let host: String
-    let port: UInt16
-    let password: String?
-    let dequeueTimeout = 2
-    let defaultTimeout = 8.0
+    public let host: String
+    public let port: UInt16
+    public let password: String?
+    public let dequeueTimeout = 2
+    public let defaultTimeout = 8.0
 
     fileprivate let redis: Redis.TCPClient
     
-    init(host: String, port: UInt16, password: String? = nil) throws {
+    public init(host: String, port: UInt16, password: String? = nil) throws {
         self.host = host
         self.port = port
         self.password = password
@@ -53,24 +53,24 @@ public final class RedisStore: Connectable {
     }
     
     @discardableResult
-    func clear<K: StoreKeyConvertible>(_ key: K) throws -> Int {
+    public func clear<K: StoreKeyConvertible>(_ key: K) throws -> Int {
         let response = try redis.command(.delete, [key.key])
         return response!.int!
     }
 }
 
 extension RedisStore {
-    func get<K: StoreKeyConvertible>(_ key: K) throws -> String? {
+    public func get<K: StoreKeyConvertible>(_ key: K) throws -> String? {
         let response = try redis.command(.get, [key.key])
         return response?.string
     }
 
-    func set<K: StoreKeyConvertible>(_ key: K, value: String) throws -> Int {
+    public func set<K: StoreKeyConvertible>(_ key: K, value: String) throws -> Int {
         let response = try redis.command(.set, [key.key, value.makeBytes()])
         return response!.int!
     }
 
-    func increment<K: StoreKeyConvertible>(_ key: K, by count: Int = 1) throws -> Int {
+    public func increment<K: StoreKeyConvertible>(_ key: K, by count: Int = 1) throws -> Int {
         let response = try redis.command(Command("INCRBY"), [key.key, String(count).makeBytes()])
         return response!.int!
     }
@@ -78,46 +78,46 @@ extension RedisStore {
 
 extension RedisStore {
     @discardableResult
-    func enqueue(_ job: [String: Any], to queue: Queue) throws -> Int {
+    public func enqueue(_ job: [String: Any], to queue: Queue) throws -> Int {
         let data = try JsonConverter.default.serialize(job)
         let response = try redis.command(Command("LPUSH"), [queue.key, data.makeBytes()])
         return response!.int!
     }
     
     @discardableResult
-    func enqueue(_ job: UnitOfWork, to queue: Queue) throws -> Int {
+    public func enqueue<T: Encodable>(_ job: T, to queue: Queue) throws -> Int {
         let data = try JSONEncoder().encode(job)
         let response = try redis.command(Command("LPUSH"), [queue.key, data.makeBytes()])
         return response!.int!
     }
-
-    func dequeue(_ queues: [Queue]) throws -> UnitOfWork? {
+    
+    public func dequeue<T: Decodable>(_ queues: [Queue]) throws -> T? {
         var params = queues.map { $0.key }
         params.append(String(dequeueTimeout).makeBytes())
-
+        
         let response = try redis.command(Command("BRPOP"), params)
         guard let array = response?.array else {
             return nil
         }
         
         let data = Data(array[1]!.bytes!)
-        return try JSONDecoder().decode(UnitOfWork.self, from: data)
+        return try JSONDecoder().decode(T.self, from: data)
     }
     
-    func size(_ queue: Queue) throws -> Int {
+    public func size(_ queue: Queue) throws -> Int {
         let response = try redis.command(Command("LLEN"), [queue.key])
         return response!.int!
     }
 }
 
 extension RedisStore {
-    func add(_ member: [String: Any], to set: Set) throws -> Int {
+    public func add(_ member: [String: Any], to set: Set) throws -> Int {
         let data = try JSONEncoder().encode(member)
         let response = try redis.command(Command("SADD"), [set.key, data.makeBytes()])
         return response!.int!
     }
 
-    func remove(_ members: [String], from set: Set) throws -> Int {
+    public func remove(_ members: [String], from set: Set) throws -> Int {
         var params = [set.key]
         members.forEach { params.append($0.makeBytes()) }
 
@@ -125,7 +125,7 @@ extension RedisStore {
         return response!.int!
     }
 
-    func members(_ set: Set) throws -> [String] {
+    public func members(_ set: Set) throws -> [String] {
         let response = try redis.command(Command("SMEMBERS"), [set.key])
         let members: [String] = response!.array!.flatMap { $0!.string! }
         return members
@@ -139,38 +139,39 @@ extension RedisStore {
 
 extension RedisStore {
     @discardableResult
-    func add(_ member: UnitOfWork, with score: SortedSetScore, to sortedSet: SortedSet) throws -> Int {
+    public func add<T: Encodable>(_ member: T, with score: SortedSetScore, to sortedSet: SortedSet) throws -> Int {
         let data = try JSONEncoder().encode(member)
         let response = try redis.command(Command("ZADD"), [sortedSet.key, score.string.makeBytes(), data.makeBytes()])
         return response!.int!
     }
 
-    @discardableResult public func remove(_ members: [String], from sortedSet: SortedSet) throws -> Int {
+    @discardableResult
+    public func remove(_ members: [String], from sortedSet: SortedSet) throws -> Int {
         var params = [sortedSet.key]
         members.forEach { params.append($0.makeBytes()) }
         let response = try redis.command(Command("ZREM"), params)
         return response!.int!
     }
 
-    func range(min: SortedSetScore, max: SortedSetScore, from sortedSet: SortedSet, offset: Int, count: Int) throws -> [String] {
+    public func range(min: SortedSetScore, max: SortedSetScore, from sortedSet: SortedSet, offset: Int, count: Int) throws -> [String] {
         let params = [sortedSet.key, min.string.makeBytes(), max.string.makeBytes(),
                       "LIMIT".makeBytes(), String(offset).makeBytes(), String(count).makeBytes()]
         let response = try redis.command(Command("ZRANGEBYSCORE"), params)
         return response!.array!.flatMap { $0!.string }
     }
 
-    func size(_ sortedSet: SortedSet) throws -> Int {
+    public func size(_ sortedSet: SortedSet) throws -> Int {
         let response = try redis.command(Command("ZCARD"), [sortedSet.key])
         return response!.int!
     }
 }
 
 extension RedisStore {
-    func pipelined() -> Redis.Pipeline<TCPInternetSocket> {
+    public func pipelined() -> Redis.Pipeline<TCPInternetSocket> {
         return redis.makePipeline()
     }
     
-    static func verify(pipelinedResponses: [Redis.Data?]) -> (successes: [Redis.Data?], errors: [Error]) {
+    static public func verify(pipelinedResponses: [Redis.Data?]) -> (successes: [Redis.Data?], errors: [Error]) {
         // workaround swift3's bug
         var successes = Array<Redis.Data?>()
         var errors = [Error]()
@@ -192,7 +193,7 @@ extension RedisStore {
     }
 }
 
-enum SortedSetScore {
+public enum SortedSetScore {
     case value(Double)
     case infinityPositive
     case infinityNegative
